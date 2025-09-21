9c6ce86c1300586f5e68e9b1dcc82e97e4eb40c2
Luis Enrique Pérez Señalin <luis.perez05@epn.edu.ec>
2025-09-21 16:37:10 -0500

    Create Logs for error in Db operations

--- app/db/session.py
+++ app/db/session.py
@@ -1,15 +1,25 @@
+import logging
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
+from fastapi import HTTPException, status
+
+logger = logging.getLogger("uvicorn.error")
 
 engine = create_engine(settings.DATABASE_URL, echo=False)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Dependencia para FastAPI
 def get_db():
-    db = SessionLocal()
     try:
+        db = SessionLocal()
         yield db
+    except Exception as e:
+        logger.error(f"Error en la conexión a la base de datos: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="Error en la base de datos"
+        )
     finally:
-        db.close()
+        if db is not None:
+            db.close()

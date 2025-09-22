f9a760d3c42b061ef58b17863f1c481474e6f3b7
Luis Enrique Pérez Señalin <luis.perez05@epn.edu.ec>
2025-09-22 12:08:41 -0500

    Add SECRET_KEY to config.py

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Kamina Backend"
     DATABASE_URL: str
-
+    SECRET_KEY: str
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"

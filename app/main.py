90abbe3df4a2096a5cf04c1dff045076c393f85d
Luis Enrique Pérez Señalin <luis.perez05@epn.edu.ec>
2025-09-21 22:27:47 -0500

    Connect book routers

--- app/main.py
+++ app/main.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI
-from app.routers import user, author
+from app.routers import user, author, book
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
 app.include_router(user.router)
 app.include_router(author.router)
+app.include_router(book.router)

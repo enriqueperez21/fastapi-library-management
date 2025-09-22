711de68fb265030c873e470f4a191a754bd71394
Luis Enrique Pérez Señalin <luis.perez05@epn.edu.ec>
2025-09-22 13:22:36 -0500

    Change Auth Login Scheme to validate fields

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,12 +1,11 @@
 from fastapi import APIRouter, Depends
-from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from app.db.session import get_db
 from app.services import auth as auth_service
-from app.schemas.auth import Token
+from app.schemas.auth import Token, LoginForm, LoginFormDep
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 @router.post("/login", response_model=Token)
-def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    return auth_service.login(db, form_data.username, form_data.password)
+def login(form_data: LoginForm = Depends(LoginFormDep), db: Session = Depends(get_db)):
+    return auth_service.login(db, form_data.email, form_data.password)
--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -1,4 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
+from fastapi import Form
+
+class LoginForm(BaseModel):
+    email: EmailStr
+    password: str
+
+def LoginFormDep(
+    email: EmailStr = Form(...),
+    password: str = Form(...)
+) -> LoginForm:
+    return LoginForm(email=email, password=password)
 
 class Token(BaseModel):
     access_token: str

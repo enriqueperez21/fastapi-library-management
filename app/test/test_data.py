5ed65b11f12e6e38db4cd08cf15ed43063bb9938
Luis Enrique Pérez Señalin <luis.perez05@epn.edu.ec>
2025-09-23 14:23:31 -0500

    Create User Tests for unique emails

--- app/test/test_data.py
+++ app/test/test_data.py
@@ -1,18 +1,34 @@
+# Data for User tests
 valid_user = {
     "name": "Test User",
     "email": "test@mail.com",
     "password": "Strong@123"
 }
 
-invalid_create_inputs = [
+invalid_user_create_inputs = [
     {"name": "Test User", "password": "Strong@123"},
     {"name": "Test User","email": "test@mail.com"},
     {"password": "Strong@123"},
 ]
 
-invalid_passwords = [
+invalid_user_passwords = [
     {"password": "strong",      "msg":"String should have at least 8 characters"},
     {"password": "stroooong",   "msg":"Value error, Password must contain at least one uppercase letter"},
     {"password": "stroooongS",  "msg":"Value error, Password must contain at least one number"},
     {"password": "stroooongS1", "msg":"Value error, Password must contain at least one special character (@$!%*?&.)"},
+]
+
+
+# Data for Author tests
+valid_author = {"name": "Robert Martin", "birthdate": "21/11/2002"}
+
+invalid_author_create_inputs = [
+    {"input": {"birthdate": "21/11/2002"}, "msg": "Field required"},
+    {"input": {"name": "Test User", "birthdate": "21-11-2002"}, "msg": "Value error, The date must be in format dd/mm/yyyy (21/11/2002)"},
+    {"input": {"name": "Test User", "birthdate": "40/11/2002"}, "msg": "Value error, The date must be in format dd/mm/yyyy (21/11/2002)"},
+]
+
+invalid_author_update_inputs = [
+    {"birthdate": "21-11-2002", "msg": "Value error, The date must be in format dd/mm/yyyy (21/11/2002)"},
+    {"birthdate": "40/11/2002", "msg": "Value error, The date must be in format dd/mm/yyyy (21/11/2002)"},
 ]
--- app/test/test_users.py
+++ app/test/test_users.py
@@ -1,5 +1,5 @@
 import pytest
-from app.test.test_data import valid_user, invalid_create_inputs, invalid_passwords
+from app.test.test_data import valid_user, invalid_user_create_inputs, invalid_user_passwords
 
 
 @pytest.fixture
@@ -7,6 +7,7 @@ def created_user(client):
     return client.post("/users", json=valid_user).json()
 
 
+#Test CRUD operations
 def test_post(client):
     response = client.post("/users", json=valid_user)
     assert response.status_code == 201
@@ -66,6 +67,7 @@ def test_delete(client, created_user):
     assert response.status_code == 404
 
 
+#Test not found errors
 def test_get_all_not_found(client):
     response = client.get("/users")
     assert response.status_code == 404
@@ -91,21 +93,35 @@ def test_delete_not_found(client):
     assert response.status_code == 404
 
 
-@pytest.mark.parametrize("invalid_user", invalid_create_inputs)
+#Test inputs errors
+def test_post_duplicate(client, created_user):
+    response = client.post("/users", json=valid_user)
+    assert response.status_code == 400
+    assert response.json()["detail"] == "Email already registered"
+
+
+def test_update_email_duplicate(client, created_user):
+    client.post("/users", json={**valid_user, "email": "test_update@mail.com"})
+    response = client.patch(f"/users/{created_user['id']}", json={"email": "test_update@mail.com"})
+    assert response.status_code == 400
+    assert response.json()["detail"] == "Email already registered"
+
+
+@pytest.mark.parametrize("invalid_user", invalid_user_create_inputs)
 def test_post_missing_fields(client, invalid_user):
     response = client.post("/users", json=invalid_user)
     assert response.status_code == 422
 
 
-@pytest.mark.parametrize("input", invalid_passwords)
-def test_post_invalid_passwords(client, input):
+@pytest.mark.parametrize("input", invalid_user_passwords)
+def test_post_invalid_user_passwords(client, input):
     response = client.post("/users", json={**valid_user, "password": input["password"]})
     assert response.status_code == 422
     assert input["msg"] in response.json()["detail"][0]["msg"]
 
 
-@pytest.mark.parametrize("input", invalid_passwords)
-def test_update_invalid_passwords(client, input):
+@pytest.mark.parametrize("input", invalid_user_passwords)
+def test_update_invalid_user_passwords(client, input):
     user_id = client.post("/users", json=valid_user).json()["id"]
     response = client.patch(f"/users/{user_id}", json={"password": input["password"]})
     assert response.status_code == 422

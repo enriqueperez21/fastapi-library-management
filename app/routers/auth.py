1a3ebb7c6c8eead6d00ea2c9d5e345d1a2360dad
Luis Enrique Pérez Señalin <luis.perez05@epn.edu.ec>
2025-09-23 23:53:19 -0500

    Create Auth route /me to verify Autorization Token

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from app.core.security import get_current_user
 from app.db.session import get_db
+from app.models.user import User
+from app.schemas.user import UserOut
 from app.services import auth as auth_service
 from app.schemas.auth import Token, LoginForm, LoginFormDep
 
@@ -9,3 +12,7 @@ router = APIRouter(prefix="/auth", tags=["auth"])
 @router.post("/login", response_model=Token)
 def login(form_data: LoginForm = Depends(LoginFormDep), db: Session = Depends(get_db)):
     return auth_service.login(db, form_data.email, form_data.password)
+
+@router.get("/me", response_model=UserOut)
+def read_current_user(current_user: User = Depends(get_current_user)):
+    return current_user
